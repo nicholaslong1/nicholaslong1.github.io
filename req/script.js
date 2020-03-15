d9475279b445fdb6438f03e784661640b0f43f63
nicholaslong1 <nicholas.long123@gmail.com>
2020-03-14 20:41:41 -0400

    Home page

---
+++ req/script.js
@@ -0,0 +1,18 @@
+$(function(){
+  //Nav resize
+  var $navs = $(".h_nav");
+  $(window).on('resize', function(){
+    var width = $(window).width();
+    // if(width<1200){
+    //   $(".break1").css("display","block")
+    //   $navs.css("display","block");
+    //   $("#name").css("display","block");
+    //
+    // } else {
+    //   $(".break1").css("display","none")
+    //   $navs.css("display","inline-block");
+    //   $("#name").css("display","inline-block");
+    // }
+  });
+
+});
